51ce0eed19e83d3ac93e656a440074efc518d67a
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2025-04-18 14:25:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,40 @@ resource "random_pet" "name" {
 
 
 
-data "terraform_remote_state" "workspace-output" {
-  backend = "remote"
+#data "terraform_remote_state" "workspace-output" {
+#  backend = "remote"
 
-  config = {
-    organization = "georgiman-tfc4b"
-    workspaces = {
-      name = "outputs"
+#  config = {
+#    organization = "georgiman-tfc4b"
+#    workspaces = {
+#      name = "outputs"
+#    }
+ # }
+#}
+
+#output "consume_sensitive_value" {
+#  value = data.terraform_remote_state.workspace-output.outputs.random_pet_name
+#}
+
+
+terraform {
+  required_providers {
+    tfe = {
+      version = "= 0.64.0"
     }
   }
 }
 
-output "consume_sensitive_value" {
-  value = data.terraform_remote_state.workspace-output.outputs.random_pet_name
+data "tfe_outputs" "foo" {
+  organization = "georgi-berchev"
+  workspace = "outputs"
+}
+
+resource "random_id" "vpc_id" {
+  keepers = {
+    # Generate a new ID any time the value of 'bar' in workspace 'my-org/my-workspace' changes.
+    bar = data.tfe_outputs.foo.values.bar
+  }
+
+  byte_length = 8
 }
